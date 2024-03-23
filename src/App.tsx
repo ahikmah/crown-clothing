16e04f8d4a197437ffe7dd798d8a14b4ad4120f4
Awaliyatul Hikmah <imadlz15@gmail.com>
2024-03-23 14:32:48 +0700

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  const categories: { id: number; title: string; imageUrl: string }[] = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  return <Directory categories={categories} />;
-}
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<h1>Hello Shop</h1>} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/routes/navigation/navigation.component.tsx
@@ -0,0 +1,25 @@
+import { Link, Outlet } from "react-router-dom";
+import CrownLogo from "../../assets/crown.svg";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link to="/" className="logo-container">
+          <img src={CrownLogo} alt="logo" className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link to="/shop" className="nav-link">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
